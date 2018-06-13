7169b4c8ed66f4dac73663fc463c410a4dcd74c0
Prinkpal Singh <turna.prink@gmail.com>
2018-06-13 17:55:37 +0530

    Assignment

---
+++ Assignment/Assignment.py
@@ -0,0 +1,28 @@
+#Question 1
+print("Hello")
+
+#Question 2
+a = "Acad"
+b = "View"
+print(a + b)
+
+#Question 3
+x = "hello"
+y = "How are you"
+z = "Man"
+print(x,y,z)
+
+#Question 4
+print('Let\'s get started')
+
+#Question 5
+s = "Acadview"
+course = "Python"
+fees = 5000
+print("%s provides a %s course whose charges are %d"%(s,course,fees))
+
+#Question 6
+name = "Tony Stark"
+salary = 1000000
+print()
+print('%s"%d'%(name,salary))
