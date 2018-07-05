b82fdf1a3f9ec36f8931ecbde4dc1a10601483b4
Prinkpal <turna.prink@gmail.com>
2018-07-05 15:52:21 +0530

    Assignment18

--- Assignment18/Assignment18.py
+++ Assignment18/Assignment18.py
@@ -1,3 +1,4 @@
+#Question1
 from tkinter import *
 root = Frame()
 root.pack()
@@ -8,18 +9,6 @@ w1 = Label(root, text="Name =")
 w1.pack(side="bottom")
 w2 = Label(root, text="Phone Number =")
 w2.pack(side="top")
-root.mainloop()
-
-
-def handleList(event):
-    label = lst.get(ACTIVE)
-    print(label)
-    ph = d.get(label)
-    global w1, w2
-    w1.config(text=label)
-    w2.config(text=ph)
-
-
 
 lst = Listbox(root)
 sbar = Scrollbar(root)
@@ -27,6 +16,14 @@ sbar.config(command=lst.yview)
 lst.config(yscrollcommand=sbar.set)
 sbar.pack(side=RIGHT, fill=Y)
 lst.pack(side=LEFT, expand=YES, fill=BOTH)
+#Question2
+def handleList(event):
+    label = lst.get(ACTIVE)
+    print(label)
+    ph = d.get(label)
+    global w1, w2
+    w1.config(text=label)
+    w2.config(text=ph)
 
 
 lst.bind('<Double-1>', handleList)
