2c45ddeeb9f0f7a27ce1899075e365f6ed91a3fd
Prinkpal <turna.prink@gmail.com>
2018-07-05 15:41:45 +0530

    Assignment18

---
+++ Data_Structures/Numpy.py
@@ -0,0 +1,38 @@
+import numpy as np
+# a = np.array([1,2,3])
+# print(type(a))
+# print(a.shape)
+#
+
+# b = np.array([[1,2,3],[4,5,6]])
+# print(b[0,0])
+
+# a = np.random.random((2,2))
+# print(a)
+a = np.arange(20).reshape(2,10)
+# print(a)
+# print(a.shape)
+# print(a.dtype.name)
+# print(a.itemsize)
+# print(a.size)
+#
+# a = np.array([[1,2,3,4],[4,5,6],[7,8,9],[10,11,12]])
+# print(a)
+#
+# b = np.array([0,2,0,1])
+# print(a[np.arange(4),b])
+# o
+
+# a = np.array([[1,2],[3,4],[5,6]])
+# bool_idx = (a>2)
+# print(bool_idx)
+
+# x = np.array([1,2],dtype=np.int64)
+# # print(x.dtype)
+# v = np.array([9,10])
+# w = np.array([11,12])
+# print(np.dot(v,w))
+#
+# x = np.array([[1,2],[3,4]])
+# print(x.transpose())
+print(not x)
